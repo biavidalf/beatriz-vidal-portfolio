df5d38effeb5a5dbedd0156062ff7fb078713bd4
Beatriz Vidal <biavidalf@gmail.com>
2024-03-01 15:05:17 -0300

    :lipstick: feat: Desktop responsiveness

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,7 +4,7 @@ import {
   Phone,
   Mail,
   Linkedin,
-  Link,
+  Languages,
 } from "lucide-react";
 import {
   Chip,
@@ -21,45 +21,63 @@ import HeroIllustration from "./../../assets/hero-illustration.svg";
 function Hero() {
   return (
     <>
-      <header className=" justif flex flex-col items-center gap-16">
-        <nav className="flex w-full flex-wrap items-center justify-between">
+      <header className="4xl:max-w-6xl m-auto flex flex-col items-center gap-16 px-5 md:h-screen md:w-full md:max-w-5xl md:justify-evenly">
+        <nav className="flex w-full flex-wrap items-center justify-between sm:hidden">
           <img src={Logo} alt="Beatriz Vidal Logo" />
           <Menu placement="bottom-end">
-            <MenuHandler>
+            <MenuHandler className="sm:hidden">
               <MenuIcons size={30} />
             </MenuHandler>
-            <MenuList className="border-deep-purple-400 bg-bg-purple-hover">
-              <MenuItem className="text-gray-200 focus:bg-deep-purple-400 focus:text-gray-100">
+            <MenuList className="border-deep-purple-400 bg-bg-purple-hover text-gray-200">
+              <MenuItem className="focus:bg-deep-purple-400 focus:text-gray-100">
                 Projects
               </MenuItem>
-              <MenuItem className="text-gray-200 focus:bg-deep-purple-400 focus:text-gray-100">
+              <MenuItem className="focus:bg-deep-purple-400 focus:text-gray-100">
                 About
               </MenuItem>
-              <MenuItem className="text-gray-200 focus:bg-deep-purple-400 focus:text-gray-100">
+              <MenuItem className="focus:bg-deep-purple-400 focus:text-gray-100">
                 Experience
               </MenuItem>
-              <MenuItem className="text-gray-200 focus:bg-deep-purple-400 focus:text-gray-100">
+              <MenuItem className="focus:bg-deep-purple-400 focus:text-gray-100">
                 Certificates
               </MenuItem>
-              <MenuItem className="text-gray-200 focus:bg-deep-purple-400 focus:text-gray-100">
+              <MenuItem className="focus:bg-deep-purple-400 focus:text-gray-100">
                 Contact
               </MenuItem>
             </MenuList>
           </Menu>
         </nav>
 
-        <main className="flex flex-col items-center justify-center gap-12">
-          <img src={HeroIllustration} className="w-2/3" />
-          <div className="flex w-full flex-col items-center gap-3">
+        <nav className="hidden font-semibold uppercase text-gray-500 *:transition-all *:delay-75 sm:flex sm:items-center sm:gap-8 sm:text-sm">
+          <a href="#Projects" className=" hover:text-gray-200">
+            Projects
+          </a>
+          <a href="#About" className=" hover:text-gray-200">
+            About
+          </a>
+          <a href="#Experience" className=" hover:text-gray-200">
+            Experience
+          </a>
+          <a href="#Certificates" className=" hover:text-gray-200">
+            Certificates
+          </a>
+          <a href="#Contact" className=" hover:text-gray-200">
+            Contact
+          </a>
+        </nav>
+
+        <main className="flex flex-col items-center justify-center gap-12 md:w-full md:flex-row-reverse md:flex-wrap md:justify-center ">
+          <img src={HeroIllustration} className="w-2/3 md:w-auto md:flex-1" />
+          <div className="flex flex-col items-center gap-3 md:flex-1 md:items-start md:gap-4">
             <Chip
               value="Hey there!"
-              className="hidden w-fit bg-bg-purple-hover text-sm normal-case"
+              className="hidden w-fit rounded-md bg-bg-purple-hover text-sm font-medium normal-case md:inline"
             />
-            <div className="flex flex-col items-center gap-3">
-              <h1 className="flex flex-wrap gap-3 text-3xl font-medium ">
-                I'm <img src={LogoName} alt="Logo name" className="w-48" />
+            <div className="flex flex-col items-center gap-3 md:items-start">
+              <h1 className="flex flex-wrap items-center gap-3 text-5xl font-medium">
+                I'm <img src={LogoName} alt="Logo name" className="h-[39px]" />
               </h1>
-              <p className="text-center text-[17px] font-medium text-gray-400">
+              <p className="text-center text-[18px] font-medium text-gray-100/60 md:text-start">
                 A 20 year old Brazilian{" "}
                 <span className="text-purple-main">Web and Salesforce</span>{" "}
                 Sales Cloud Developer that takes the client needs and a nice
@@ -68,23 +86,25 @@ function Hero() {
             </div>
 
             <div className="mt-4 flex flex-wrap items-center gap-5">
-              <Button
+              {/* <Button
                 size="lg"
                 className="bg-gradient-to-r from-deep-purple-600 to-deep-purple-400 text-base font-semibold normal-case tracking-wide"
               >
                 Best Projects
-              </Button>
+              </Button> */}
+              <button className="rounded-md bg-gradient-to-r from-[#5d55ce] via-purple-main to-[#8e87ed] px-7 py-3 text-[20px] font-medium normal-case tracking-wide">
+                Best Projects
+              </button>
               <a
                 href=""
-                className="group font-medium hover:border-b hover:border-purple-main "
+                className="border-b border-gray-400 text-gray-400 transition-all delay-75 hover:border-gray-50 hover:text-gray-50"
               >
-                Open{" "}
-                <span className="border-b group-hover:border-none">resume</span>
+                Open resume
               </a>
             </div>
           </div>
 
-          <ul className="mt-3 flex flex-wrap items-center justify-center gap-3 text-gray-400">
+          <ul className="mt-3 flex flex-wrap items-center justify-center gap-3 text-gray-400 md:hidden">
             <li>
               <Github strokeWidth={1.5} />
             </li>
@@ -97,8 +117,29 @@ function Hero() {
             <li>
               <Linkedin strokeWidth={1.5} />
             </li>
+            <li>
+              <Languages strokeWidth={1.5} />
+            </li>
           </ul>
         </main>
+
+        <ul className="mt-3 hidden flex-wrap items-center gap-5 self-end text-gray-400 md:flex">
+          <li>
+            <Github strokeWidth={1.5} />
+          </li>
+          <li>
+            <Phone strokeWidth={1.5} />
+          </li>
+          <li>
+            <Mail strokeWidth={1.5} />
+          </li>
+          <li>
+            <Linkedin strokeWidth={1.5} />
+          </li>
+          <li className="ml-6">
+            <Languages strokeWidth={1.5} />
+          </li>
+        </ul>
       </header>
     </>
   );
