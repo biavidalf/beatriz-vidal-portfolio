ae814f16ccce499a12acf36bf53af6da34138f38
Beatriz Vidal <biavidalf@gmail.com>
2024-03-05 10:59:34 -0300

    :sparkles: feat: Certificates section

---
+++ src/components/BlogCard/index.jsx
@@ -0,0 +1,73 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Avatar,
+  Tooltip,
+  Button,
+} from "@material-tailwind/react";
+
+import Tag from "../Tag";
+
+export function BlogCard() {
+  return (
+    <Card className="max-w-full overflow-hidden rounded-md border border-stroke bg-bg-glass/50 text-gray-100 lg:max-w-sm">
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="m-0 rounded-none"
+      >
+        <img
+          src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+          alt="ui/ux review check"
+        />
+      </CardHeader>
+      <CardBody className=" pb-0">
+        <Typography variant="h5" className="text-gray-200">
+          OSF Academy
+        </Typography>
+        <Typography
+          variant="paragraph"
+          className="mt-3 font-normal text-gray-300"
+        >
+          Because it&apos;s about motivating the doers. Because I&apos;m here to
+          follow my dreams and inspire others.
+        </Typography>
+
+        <div className="mt-2 flex flex-wrap items-center gap-2">
+          <Tag content="5 Weeks" />
+          <Tag content="Salesforce" />
+          <Tag content="English" />
+        </div>
+      </CardBody>
+      <CardFooter className="flex items-center">
+        <a href="#" className="inline-block">
+          <Button
+            size="sm"
+            variant="text"
+            className="flex items-center gap-2 rounded text-gray-200 hover:bg-bg-purple-hover"
+          >
+            See details
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </Button>
+        </a>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/components/Sections/Certificates/index.jsx
@@ -0,0 +1,73 @@
+import { SectionTitle, TextHighlight } from "../../Text";
+import Tag from "../../Tag";
+import { BlogCard } from "../../BlogCard";
+import Pagination from "../../Pagination";
+
+import OsfAcademy from "../../../assets/courses/osf-academy.png";
+import { IconButton } from "@material-tailwind/react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+function Certificate() {
+  return (
+    <div className="overflow-clip rounded-md border border-stroke bg-bg-glass/40 lg:w-1/2">
+      <img src={OsfAcademy} alt="" />
+      <div className="p-6">
+        <h1 className="text-xl font-medium lg:text-2xl">OSF Academy</h1>
+        <p className="mt-2 lg:text-lg">
+          Salesforce Commerce Cloud training by the canadian roman company OSF
+          Digital
+        </p>
+        <div className="mt-4 flex flex-row flex-wrap gap-2">
+          <Tag content="5 weeks" />
+          <Tag content="english" />
+          <Tag content="salesforce" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Certificates() {
+  return (
+    <section className="bg-bg-mesh h-fit w-full !max-w-none bg-no-repeat">
+      <div className="mx-auto flex max-w-5xl flex-col gap-y-5 py-20">
+        <SectionTitle content="Certificates" />
+        <p className="text-justify text-lg text-gray-100/75 lg:text-xl">
+          During my <TextHighlight>journey</TextHighlight>, I’ve earned a
+          variaty of certificates in web development, Salesforce, and design. I
+          firmly believe in the power of continuous learning—constantly{" "}
+          <TextHighlight>
+            seeking new knowledge and refining my skills
+          </TextHighlight>
+          . These certifications represent my{" "}
+          <TextHighlight>commitment to growth</TextHighlight> and excellence in
+          the ever-evolving world of technology.
+        </p>
+        <div className="mt-4 flex flex-col gap-y-7 lg:flex-row lg:flex-wrap lg:items-center lg:justify-center lg:gap-x-4">
+          {window.innerWidth <= 960 ? (
+            ""
+          ) : (
+            <IconButton
+              size="sm"
+              className="bg-transparent text-gray-300 hover:bg-bg-purple-hover focus:ring-purple-main"
+            >
+              <ChevronLeft size={24} />
+            </IconButton>
+          )}
+          <BlogCard />
+          <BlogCard />
+          {window.innerWidth <= 960 ? (
+            <Pagination className="self-center" />
+          ) : (
+            <IconButton
+              size="sm"
+              className="bg-transparent text-gray-300 hover:bg-bg-purple-hover focus:ring-purple-main"
+            >
+              <ChevronRight size={24} />
+            </IconButton>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
